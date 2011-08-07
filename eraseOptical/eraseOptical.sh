fed20b604adf8570ed63dd8a77144a397d6bd385
Jeffrey Gifford <JeffreyGifford@gmail.com>
2011-08-07 16:58:17 -0300

    added some notes for future version

--- eraseOptical/eraseOptical.sh
+++ eraseOptical/eraseOptical.sh
@@ -2,4 +2,7 @@
 
 # Erases an optical (re-writable) cd (or DVD)
 
+# Add a quick check (do you really want to do this?)
+# Add a check,wait,check loop to see if the drive isn't in. Max 5 retries or something
+
 /usr/bin/sudo /usr/sbin/diskutil eraseOptical quick /dev/disk1
